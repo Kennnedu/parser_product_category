099c310beca258285e5593be63082022baf205f5
Roman <sir.kenda@yandex.ru>
2018-05-12 11:27:51 +0300

    rubocop

--- category_extractor.rb
+++ category_extractor.rb
@@ -1,18 +1,17 @@
 class CategoryExtractor
   include ParserHelper
-
   BASE_URL = 'https://www.viovet.co.uk'.freeze
   HEADER_CSV = %w(Name Amount Image DeliveryDate Code).freeze
 
-  def initialize(category_url, filename)
+  def initialize(category_url, filename = 'extracted_data')
     @current_page = save_page(category_url)
     @filename = filename
-    @multiproduct_extractor = MultiproductExtractor.new()
+    @multiproduct_extractor = MultiproductExtractor.new
   end
 
   def self.call(category_url, filename)
     return unless category_url
-    new(category_url, filename = 'extracted_data').call
+    new(category_url, filename).call
   end
 
   def call
@@ -28,17 +27,16 @@ class CategoryExtractor
     CSV.open("#{@filename}.csv", 'wb') do |csv|
       csv << HEADER_CSV
 
-      get_links_multiproducts.each do |links_multiproduct|
+      parse_links_multiproducts.each do |links_multiproduct|
         @multiproduct_extractor.page = save_page(links_multiproduct)
         @multiproduct_extractor.call(csv)
       end
     end
   end
 
-  def get_links_multiproducts
+  def parse_links_multiproducts
     @current_page.css('a.grid-box').map { |tag| BASE_URL + tag['href'] }
   end
-
 
   def exist_next_page?
     @current_page.xpath(".//div[@class='pagination']").to_s.empty?
--- product_extractor.rb
+++ product_extractor.rb
@@ -1,5 +1,4 @@
 class ProductExtractor
-
   attr_accessor :id, :page
 
   def initialize
@@ -12,12 +11,12 @@ class ProductExtractor
   end
 
   def parse_name
-    @result[:name] = name_part_1 + ' ' + name_part_2 
+    @result[:name] = name_part_1 + ' ' + name_part_2
   end
 
   def parse_price
-    @result[:price] = @page.xpath(".//ul[@id='product-select-list']/li[@data-product_id ='#{@id}']/span[@class='price']").text
-                        .to_s.strip.slice(1..-1).to_f
+    @result[:price] = @page.xpath(".//ul[@id='product-select-list']/li[@data-product_id ='#{@id}']/span[@class='price']")
+                           .text.to_s.strip.slice(1..-1).to_f
   end
 
   def parse_code
@@ -26,12 +25,13 @@ class ProductExtractor
 
   def parse_image_url
     img = @page.xpath(".//div[@class='gridbox three-eighths']/div/img[@id='product_image_#{@id}']/@src").to_s
-    @result[:image_url] =  img.empty? ? without_image_url : img
+    @result[:image_url] = img.empty? ? without_image_url : img
   end
 
   def parse_delivery_time
-   @result[:delivery_time] = @page.xpath(".//div[@class='product_#{@id}_details product_details_list']/div/p[@class='notification_in-stock']")
-                              .text.to_s.strip
+    @result[:delivery_time] =
+      @page.xpath(".//div[@class='product_#{@id}_details product_details_list']/div/p[@class='notification_in-stock']")
+           .text.to_s.strip
   end
 
   private
@@ -43,7 +43,7 @@ class ProductExtractor
   def name_part_2
     @page.search(".//span[@class='clearance_product_label']").remove
     @page.xpath(".//ul[@id='product-select-list']/li[@data-product_id ='#{@id}']/span[@class='name']")
-                      .text.to_s.strip
+         .text.to_s.strip
   end
 
   def without_image_url
